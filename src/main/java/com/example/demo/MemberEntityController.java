fb559b60c874ed8ee750b1cf7c4f291d431d85ef
baejunil <bjo6300@naver.com>
2024-01-27 02:49:01 +0900

    controller의 response status를 변경한다.

--- src/main/java/com/example/demo/MemberEntityController.java
+++ src/main/java/com/example/demo/MemberEntityController.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import java.net.URI;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,21 +15,24 @@ public class MemberEntityController {
     @PostMapping("/save1")
     public ResponseEntity<Void> save1(String name) {
         memberEntityService.saveMember1(name);
+        URI uri = URI.create("/save1");
 
-        return ResponseEntity.noContent().build();
+        return ResponseEntity.created(uri).build();
     }
 
     @PostMapping("/save2")
     public ResponseEntity<Void> save2(String name) {
         memberEntityService.saveMember2(name);
+        URI uri = URI.create("/save2");
 
-        return ResponseEntity.noContent().build();
+        return ResponseEntity.created(uri).build();
     }
 
     @PostMapping("/save3")
     public ResponseEntity<Void> save3(String name) {
         memberEntityService.saveMember3(name);
+        URI uri = URI.create("/save1");
 
-        return ResponseEntity.noContent().build();
+        return ResponseEntity.created(uri).build();
     }
 }
